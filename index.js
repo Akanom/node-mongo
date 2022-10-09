1253749202676bb31ba6743e02b8e5e90eb55922
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2022-10-09 06:17:25 +0200

    third mongo commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert").strict;
-const dboper = require("./operations");
 
 const url = "mongodb://localhost:27017/";
 const dbname = "nucampsite";
@@ -16,39 +15,19 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
     assert.strictEqual(err, null);
     console.log("Dropped Collection", result);
 
-    dboper.insertDocument(
-      db,
+    const collection = db.collection("campsites");
+
+    collection.insertOne(
       { name: "Breadcrumb Trail Campground", description: "Test" },
-      "campsites",
-      (result) => {
+      (err, result) => {
+        assert.strictEqual(err, null);
         console.log("Insert Document:", result.ops);
 
-        dboper.findDocument(db, "campsites", (docs) => {
+        collection.find().toArray((err, docs) => {
+          assert.strictEqual(err, null);
           console.log("Found Documents:", docs);
 
-          dboper.updateDocument(
-            db,
-            { name: "Breadcrumb Trail Campground" },
-            { description: "Updated Test Description" },
-            "campsites",
-            (result) => {
-              console.log("Updated Document Count:", result.result.nModified);
-              dboper.findDocument(db, "campsites", (docs) => {
-                console.log("Found Documents:", docs);
-
-                dboper.removeDocument(
-                  db,
-                  { name: "Breadcrumb Trail Campground" },
-                  "campsites",
-                  (result) => {
-                    console.log("Deleted Document Count:", result.deletedCount);
-
-                    client.close();
-                  }
-                );
-              });
-            }
-          );
+          client.close();
         });
       }
     );
